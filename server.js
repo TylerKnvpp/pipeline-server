1ebeab1d33200199d5a246746abf77a2ae66011b
Tyler Knapp <tgknapp11@gmail.com>
2020-08-19 15:32:40 -0700

    auth router

--- server.js
+++ server.js
@@ -40,8 +40,8 @@ connection.once("open", () => {
 //   })
 // );
 
-// const authRouter = require("./routes/auth");
-// app.use("/auth", authRouter);
+const authRouter = require("./routes/auth");
+app.use("/auth", authRouter);
 
 // const usersRouter = require("./routes/users");
 // app.use("/users", usersRouter);
